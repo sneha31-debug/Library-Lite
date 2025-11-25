f2dbe483a05339b7897c74b0a0def6193f42314d
sneha31-degug <snehachepurwar6@gmail.com>
2025-11-25 22:39:53 +0530

    frontend setup done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,13 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'  // ✅ important for Tailwind to apply
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
